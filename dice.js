bcfd3f332214e67a1f290c7517b8068ce91e9e1a
David V <jdavidverdugo7@gmail.com>
2023-08-03 00:05:28 +1200

    alert removed

--- dice.js
+++ dice.js
@@ -1,12 +1,3 @@
-// dice.js
-
-//function rolldices() {
-//let diceSidesSelect = document.getElementById("dicesides");
-//let diceSides = parseInt(diceSidesSelect.value, 10);
-
-//let randomNumber = Math.floor(Math.random() * diceSides) + 1;
-// document.getElementById("diceface").innerHTML = dicefase;}
-
 function rolldices() {
   let diceSidesSelect = document.getElementById("dicesides");
   let diceSides = parseInt(diceSidesSelect.value, 10);
