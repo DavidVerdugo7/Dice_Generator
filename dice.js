e36b949095eecfcb93f6cc1e288d24ec26d46a1f
David V <jdavidverdugo7@gmail.com>
2023-08-02 23:28:01 +1200

    update as Anthony adviced

--- dice.js
+++ dice.js
@@ -1,11 +1,16 @@
 // dice.js
 
+//function rolldices() {
+//let diceSidesSelect = document.getElementById("dicesides");
+//let diceSides = parseInt(diceSidesSelect.value, 10);
+
+//let randomNumber = Math.floor(Math.random() * diceSides) + 1;
+// document.getElementById("diceface").innerHTML = dicefase;}
+
 function rolldices() {
   let diceSidesSelect = document.getElementById("dicesides");
   let diceSides = parseInt(diceSidesSelect.value, 10);
 
   let randomNumber = Math.floor(Math.random() * diceSides) + 1;
-  alert(
-    "You rolled a " + randomNumber + " on the " + diceSides + " sided dice!"
-  );
+  document.getElementById("diceface").innerHTML = randomNumber; // Display the result
 }
